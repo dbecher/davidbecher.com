b2ff1505d936c31c169d9e2feba5a740b0de7b09
David Becher <dbecher@me.com>
2026-01-13 19:31:58 -0800

    re-enable photoset type

--- wp-content/plugins/photoset/photoset.php
+++ wp-content/plugins/photoset/photoset.php
@@ -33,6 +33,8 @@ function photoset_register_photo_post_type() {
     register_post_type( 'photoset', $args );
 }
 
+add_action( 'init', 'photoset_register_photo_post_type' );
+
 // Modify Query Loop block queries to include photosets when the block has the specific class
 add_filter('query_loop_block_query_vars', 'add_photoset_to_query_loop');
 function add_photoset_to_query_loop($query, $block = null) {
